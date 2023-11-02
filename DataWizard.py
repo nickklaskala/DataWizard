35a3726a2cbc9079b985578157e4891afad2e0e4
nickklaskala <57536239+nickklaskala@users.noreply.github.com>
2023-11-02 14:23:44 -0400

    Update DataWizard.py

--- DataWizard.py
+++ DataWizard.py
@@ -12,13 +12,14 @@ from collections import OrderedDict
 import webbrowser
 from collections import OrderedDict
 from csv import DictReader
-
+import io
 
 def getDelimiter(text):
 	dct={'|':0}
 	for i in set(text.splitlines()[0]):
-		if i not in ('abcdefghijklmnopqrstuvwxqyzABCDEFGHIJKLMNOPQRSTUVWXQYZ0123456789=:_- "().[]{}/\\'):
+		if i not in ('abcdefghijklmnopqrstuvwxqyzABCDEFGHIJKLMNOPQRSTUVWXQYZ0123456789''=:_- "().[]{}/\\'):
 			dct[i]=text.splitlines()[0].count(i)
+	print((max(dct,key=dct.get)))
 	return (max(dct,key=dct.get))
 
 
@@ -26,16 +27,17 @@ def maske(element):
 	if isinstance(element,list):
 		return [maske(el) for el in element]
 	elif isinstance(element,str):
-		newElement=''
-		for char in element:
-			if char in ('abcdefghijklmnopqrstuvwxqyz'):
-				newElement+='a'
-			elif char in ('ABCDEFGHIJKLMNOPQRSTUVWXQYZ'):
-				newElement+='A'
-			elif char in ('0123456789'):
-				newElement+='9'
-			else:
-				newElement+=char
+		newElement=re.sub('[0-9]', '9',re.sub('[a-z]', 'a',re.sub('[A-Z]', 'A', element)))
+
+		# for char in element:
+			# if char in ('abcdefghijklmnopqrstuvwxqyz'):
+			# 	newElement+='a'
+			# elif char in ('ABCDEFGHIJKLMNOPQRSTUVWXQYZ'):
+			# 	newElement+='A'
+			# elif char in ('0123456789'):
+			# 	newElement+='9'
+			# else:
+			# 	newElement+=char
 		return newElement
 
 
@@ -121,8 +123,9 @@ class dataGrid:
 	def getDelimiter(self,text):
 		dct={'|':0}
 		for i in set(text.splitlines()[0]):
-			if i not in ('abcdefghijklmnopqrstuvwxqyzABCDEFGHIJKLMNOPQRSTUVWXQYZ0123456789=:_- "().[]{}/\\'):
+			if i not in ('abcdefghijklmnopqrstuvwxqyzABCDEFGHIJKLMNOPQRSTUVWXQYZ0123456789\'=:_- "().[]{}/\\'):
 				dct[i]=text.splitlines()[0].count(i)
+		print((max(dct,key=dct.get)))
 		return (max(dct,key=dct.get))
 
 	def getGrid(self,text,delimiter,quotechar):
@@ -143,7 +146,6 @@ class dataGrid:
 	def getSampleGrid(self):
 		self.sampleGrid= [['' for x in y] for y in self.grid[1:]]
 		self.sampleGrid.insert(0,self.grid[0])
-		print(self.sampleGrid)
 		for x in range(len(self.grid[0])):
 			col=[y[x] for y in self.grid[1:]]
 			col=sorted(list(set(col)))
@@ -157,7 +159,6 @@ class dataGrid:
 		else:
 			maxColWidth=self.getMaxColumnWidth(grid)
 			rst= '\n'.join([delimiter.join("{:<{width}}".format(col, width=maxColWidth[index]) for index, col in enumerate(row)) for row in grid])
-			print(maxColWidth)
 		return rst
 
 	def pivotGrid(self):
@@ -323,6 +324,35 @@ class datawizardlowercasesqlkeywordsCommand(sublime_plugin.TextCommand):
 		runEdit(self, edit)
 
 
+
+class datawizarduppercasesqlkeywordsCommand(sublime_plugin.TextCommand):
+	def format(self,text):
+		lines=text.splitlines()
+		#sql server
+		# words = ['if','cascade','serial','integer','owner','to','grant','serial','integer','while','deallocate','row_number','else','then','string_agg','returns','bit','nolock','use','go','clustered','after','nocount','on','raiserror','instead','of','enable','trigger','upper','isnull','lower','rank','over','partition','when','datediff','cast','convert','add','constraint','alter','column','table','all','and','any','as','asc','backup','database','between','case','check','create','index','or','replace','view','procedure','unique','default','delete','desc','distinct','drop','exec','exists','foreign','key','from','full','outer','join','group','by','having','in','inner','insert','into','select','is','null','not','left','like','limit','order','primary','right','rownum','top','set','truncate','union','update','values','where','cross','date','datetime','execute','max','concat','for','fetch','next','close','open','varchar','int','object','declare','end','try','print','catch','with','begin','proc']
+		#postgres
+		words=['abort','abs','absent','absolute','access','according','acos','action','ada','add','admin','after','aggregate','all','allocate','also','alter','always','analyse','analyze','and','any','are','array','array_agg','array_max_cardinality','as','asc','asensitive','asin','assertion','assignment','asymmetric','at','atan','atomic','attach','attribute','attributes','authorization','avg','backward','base64','before','begin','begin_frame','begin_partition','bernoulli','between','bigint','binary','bit','bit_length','blob','blocked','bom','boolean','both','breadth','by','cache','call','called','cardinality','cascade','cascaded','case','cast','catalog','catalog_name','ceil','ceiling','chain','chaining','char','char_length','character','character_length','character_set_catalog','character_set_name','character_set_schema','characteristics','characters','check','checkpoint','class','class_origin','classifier','clob','close','cluster','coalesce','cobol','collate','collation','collation_catalog','collation_name','collation_schema','collect','column','column_name','columns','command_function','command_function_code','comment','comments','commit','committed','compression','concurrently','condition','condition_number','conditional','configuration','conflict','connect','connection','connection_name','constraint','constraint_catalog','constraint_name','constraint_schema','constraints','constructor','contains','content','continue','control','conversion','convert','copy','corr','corresponding','cos','cosh','cost','count','covar_pop','covar_samp','create','cross','cube','cume_dist','current','current_catalog','current_date','current_default_transform_group','current_path','current_role','current_row','current_schema','current_time','current_timestamp','current_transform_group_for_type','current_user','cursor','cursor_name','cycle','data','database','datalink','date','datetime_interval_code','datetime_interval_precision','day','db','deallocate','dec','decfloat','decimal','declare','default','defaults','deferrable','deferred','define','defined','definer','degree','delete','delimiter','delimiters','dense_rank','depends','depth','deref','derived','desc','describe','descriptor','detach','deterministic','diagnostics','dictionary','disable','discard','disconnect','dispatch','distinct','dlnewcopy','dlpreviouscopy','dlurlcomplete','dlurlcompleteonly','dlurlcompletewrite','dlurlpath','dlurlpathonly','dlurlpathwrite','dlurlscheme','dlurlserver','dlvalue','do','document','domain','double','drop','dynamic','dynamic_function','dynamic_function_code','each','element','else','empty','enable','encoding','encrypted','end','end_frame','end_partition','enforced','enum','equals','error','escape','event','every','except','exception','exclude','excluding','exclusive','exec','execute','exists','exp','explain','expression','extension','external','extract','false','family','fetch','file','filter','final','finalize','finish','first','first_value','flag','float','floor','following','for','force','foreign','format','fortran','forward','found','frame_row','free','freeze','from','fs','fulfill','full','function','functions','fusion','general','generated','get','global','go','goto','grant','granted','greatest','group','grouping','groups','handler','having','header','hex','hierarchy','hold','hour','id','identity','if','ignore','ilike','immediate','immediately','immutable','implementation','implicit','import','in','include','including','increment','indent','index','indexes','indicator','inherit','inherits','initial','initially','inline','inner','inout','input','insensitive','insert','instance','instantiable','instead','int','integer','integrity','intersect','intersection','interval','into','invoker','is','isnull','isolation','join','json','json_array','json_arrayagg','json_exists','json_object','json_objectagg','json_query','json_table','json_table_primitive','json_value','keep','key','key_member','key_type','keys','label','lag','language','large','last','last_value','lateral','lead','leading','leakproof','least','left','length','level','library','like','like_regex','limit','link','listagg','listen','ln','load','local','localtime','localtimestamp','location','locator','lock','locked','log','log10','logged','lower','map','mapping','match','match_number','match_recognize','matched','matches','materialized','max','maxvalue','measures','member','merge','message_length','message_octet_length','message_text','method','min','minute','minvalue','mod','mode','modifies','module','month','more','move','multiset','mumps','name','names','namespace','national','natural','nchar','nclob','nested','nesting','new','next','nfc','nfd','nfkc','nfkd','nil','no','none','normalize','normalized','not','nothing','notify','notnull','nowait','nth_value','ntile','nullif','nulls','number','numeric','object','occurrences_regex','octet_length','octets','of','off','offset','oids','old','omit','on','one','only','open','operator','option','options','or','order','ordering','ordinality','others','out','outer','output','over','overflow','overlaps','overlay','overriding','owned','owner','pad','parallel','parameter','parameter_mode','parameter_name','parameter_ordinal_position','parameter_specific_catalog','parameter_specific_name','parameter_specific_schema','parser','partial','partition','pascal','pass','passing','passthrough','password','past','path','pattern','per','percent','percent_rank','percentile_cont','percentile_disc','period','permission','permute','placing','plan','plans','pli','policy','portion','position','position_regex','power','precedes','preceding','precision','prepare','prepared','preserve','primary','prior','private','privileges','procedural','procedure','procedures','program','prune','ptf','public','publication','quote','quotes','range','rank','read','reads','real','reassign','recheck','recovery','recursive','ref','references','referencing','refresh','regr_avgx','regr_avgy','regr_count','regr_intercept','regr_r2','regr_slope','regr_sxx','regr_sxy','regr_syy','reindex','relative','release','rename','repeatable','replace','replica','requiring','reset','respect','restart','restore','restrict','result','return','returned_cardinality','returned_length','returned_octet_length','returned_sqlstate','returning','returns','revoke','right','role','rollback','rollup','routine','routine_catalog','routine_name','routine_schema','routines','row','row_count','row_number','rows','rule','running','savepoint','scalar','scale','schema','schema_name','schemas','scope','scope_catalog','scope_name','scope_schema','scroll','search','second','section','security','seek','select','selective','self','sensitive','sequence','sequences','serializable','server','server_name','session','session_user','set','setof','sets','share','show','similar','simple','sin','sinh','size','skip','smallint','snapshot','some','source','space','specific','specific_name','specifictype','sql','sqlcode','sqlerror','sqlexception','sqlstate','sqlwarning','sqrt','stable','standalone','start','state','statement','static','statistics','stddev_pop','stddev_samp','stdin','stdout','storage','stored','strict','string','strip','structure','style','subclass_origin','submultiset','subscription','subset','substring','substring_regex','succeeds','sum','support','symmetric','sysid','system','system_time','system_user','table','table_name','tables','tablesample','tablespace','tan','tanh','temp','template','temporary','text','then','through','ties','time','timestamp','timezone_hour','timezone_minute','to','token','top_level_count','trailing','transaction','transaction_active','transactions_committed','transactions_rolled_back','transform','transforms','translate','translate_regex','translation','treat','trigger','trigger_catalog','trigger_name','trigger_schema','trim','trim_array','true','truncate','trusted','type','types','uescape','unbounded','uncommitted','unconditional','under','unencrypted','union','unique','unknown','unlink','unlisten','unlogged','unmatched','unnamed','unnest','until','untyped','update','upper','uri','usage','user','user_defined_type_catalog','user_defined_type_code','user_defined_type_name','user_defined_type_schema','using','utf16','utf32','utf8','vacuum','valid','validate','validator','value','value_of','values','var_pop','var_samp','varbinary','varchar','variadic','varying','verbose','version','versioning','view','views','volatile','when','whenever','where','whitespace','width_bucket','window','with','within','without','work','wrapper','write','xml','xmlagg','xmlattributes','xmlbinary','xmlcast','xmlcomment','xmlconcat','xmldeclaration','xmldocument','xmlelement','xmlexists','xmlforest','xmliterate','xmlnamespaces','xmlparse','xmlpi','xmlquery','xmlroot','xmlschema','xmlserialize','xmltable','xmltext','xmlvalidate','year','yes','zone']
+
+		uppercase = lambda x: x.group(1).upper()
+		test = '\b({})\b'.format('|'.join(words))
+		re_replace = re.compile(r'\b({})\b'.format('|'.join(words)),re.IGNORECASE)
+		cflag=0
+		for i in range(0,len(lines)):
+			if '/*' in lines[i] and '*/' not in lines[i]:
+				cflag=1
+			if '*/' in lines[i]:
+				cflag=0
+			if cflag==1:
+				continue
+			temp=lines[i].split('--')
+			temp[0]=re_replace.sub(uppercase, temp[0])
+			lines[i]='--'.join(temp)
+		return '\n'.join(lines)
+
+	def run(self, edit):
+		runEdit(self, edit)
+
+
 class datawizardpyvartotextCommand(sublime_plugin.TextCommand):
 	def format(self,text):
 		return text.replace('\\n','\n').replace('\\t','\t')
@@ -429,9 +459,9 @@ class datawizarddistinctcolumnsCommand(sublime_plugin.TextCommand):
 
 class datawizarddistinctcolumnformatsCommand(sublime_plugin.TextCommand):
 	def format(self,text):
-		a=dataGrid(text)
-		for index,row in enumerate(a.grid[1:],start=1):
-			a.grid[index]=maske(row)
+		lines=text.splitlines()
+		first_line=lines.pop(0)
+		a=dataGrid(first_line+'\n'+maske('\n'.join(lines)))
 		a.getSampleGrid()
 
 		rst=a.constructTextFromGrid(a.sampleGrid,a.delimiter,a.sampleMaxColWidth)
@@ -702,7 +732,7 @@ class datawizardformatcsvtonestedjsonCommand(sublime_plugin.TextCommand):
 
 
 
-class datawizardjsontoictCommand(sublime_plugin.TextCommand):
+class datawizardjsontodictCommand(sublime_plugin.TextCommand):
 	def format(self,text):
 		json_data=json.loads(text)
 		return str(json_data)
@@ -720,4 +750,35 @@ class datawizarddicttojsonCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		runEdit(self, edit)
 
- 
+
+
+
+class datawizardjsontocsvCommand(sublime_plugin.TextCommand):
+	def format(self,text):
+
+		dict_list=[json.loads(line) for line in text.splitlines()]
+		fieldnames = set()
+		for d in dict_list:
+			fieldnames.update(d.keys())
+		fieldnames = sorted(fieldnames)    # sort the fieldnames...
+
+		# produce the csv file 
+		mem_file = io.StringIO()
+		wr = csv.DictWriter(mem_file, fieldnames)
+		wr.writeheader()
+		wr.writerows(dict_list)
+
+		rst=mem_file.getvalue().replace('\r','')
+
+		return rst
+
+	def run(self, edit):
+		runEdit(self, edit)
+
+
+
+
+
+
+
+
